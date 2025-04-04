f2c2c5c489e128e2bff19db57c2d173e1d700436
rkbart <rk_bart@yahoo.com>
2025-04-05 00:17:44 +0800

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @comment_galing_sa_articles = @article.comments.build
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: "Article created successfully."
+    else
+      flash.alert = "Unable to create article."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article updated successfully."
+    else
+      flash.alert = "Unable to update article."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,52 @@
+class CommentsController < ApplicationController
+  before_action :set_article
+  before_action :set_comment, only: %i[show edit update destroy]
+
+  def index
+    @comments = @article.comments
+  end
+
+  def create
+    @comment = @article.comments.build(comment_params)
+    
+    if @comment.save
+      redirect_to @article, notice: "Comment was successfully created."
+    else
+      flash.alert = "Comment was not created."
+      render "articles/show", status: :unprocessable_entity
+      # redirect_to @article
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @article, notice: "Comment updated successfully."
+    else  
+      flash.alert = 'Unable to update comment.'  
+      render :edit, status: :unprocessable_entity
+    end  
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @article, notice: "Comment was successfully deleted."
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def set_comment
+    @comment = @article.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
